1a6b44edea275697e82dcff3c78d6e73c37c56fc
A1ex-Fu <alex.aj.fu@gmail.com>
2025-12-08 15:33:34 -0800

    9 alt ver

--- profile.py
+++ profile.py
@@ -14,29 +14,36 @@ request = pc.makeRequestRSpec()
 IMAGE = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD'
 
 # Define the number of nodes.
-NUM_NODES = 4
+NUM_NODES = 10
 
 # Create a LAN to connect all nodes.
 lan = request.LAN("lan")
-lan.bandwidth = 25600000  # Set bandwidth to 25 Gbps (in Kbps)
+
+# Set bandwidth to 25 Gbps (25,000,000 Kbps).
+# Note: r320 nodes have 40Gbps physical links. 
+# CloudLab will enforce this 25Gbps limit via software traffic shaping.
+lan.bandwidth = 25000000 
 
 # Create the nodes and add them to the LAN.
 for i in range(NUM_NODES):
     node = request.RawPC("node-{}".format(i+1))
-    # node.hardware_type = 'xl170'
+    
+    # Use the 'r320' hardware type found in the APT cluster.
+    node.hardware_type = 'r320'
     node.disk_image = IMAGE
 
     # Add an interface to the node and connect to LAN
     iface = node.addInterface("if1")
     lan.addInterface(iface)
 
-    # Assign roles: node-1 is the client, others are Paxos replicas
-    if i == 0:
-        node.addService(rspec.Execute(shell="sh", command="/local/repository/setup_client.sh"))
-    else:
-        node.addService(rspec.Execute(shell="sh", command="/local/repository/setup_replica.sh"))
+    # # Assign roles: node-1 is the client, others are Paxos replicas
+    # if i == 0:
+    #     node.addService(rspec.Execute(shell="sh", command="/local/repository/setup_client.sh"))
+    # else:
+    #     node.addService(rspec.Execute(shell="sh", command="/local/repository/setup_replica.sh"))
 
     # Performance tuning: reserve cores and disable irqbalance
+    # These settings are compatible with the r320's Intel Sandy Bridge architecture.
     node.addService(rspec.Execute(shell="sh", command="echo 1 > /sys/bus/workqueue/devices/writeback/cpumask"))
     node.addService(rspec.Execute(shell="sh", command="echo 2 > /sys/bus/workqueue/devices/writeback/cpumask"))
     node.addService(rspec.Execute(shell="sh", command="systemctl disable irqbalance"))
